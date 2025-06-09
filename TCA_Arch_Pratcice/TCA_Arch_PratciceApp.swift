32bfa5ea3998a9aa6d775b6acfea9b6d34d99342
Denis Denisov <denis.denisov8701@gmail.com>
2025-06-09 15:47:02 +0200

    Initial Commit

---
+++ TCA_Arch_Pratcice/TCA_Arch_PratciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Arch_PratciceApp.swift
+//  TCA_Arch_Pratcice
+//
+//  Created by Denis Denisov on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Arch_PratciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
